05f2026510aa9ca8f5b1f415a51a76171ed20ae3
Maksym Gomon <Maksym.Gomon@gmail.com>
2016-06-16 12:05:45 +0300

    Change default route

--- TestTask/App_Start/RouteConfig.cs
+++ TestTask/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TestTask
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "User", action = "List", id = UrlParameter.Optional }
             );
         }
     }
